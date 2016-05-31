1b3acf659860b2e50cd6f0a599a184b13fe5be02
Jaiveer Kothari <jaiveerkothari@yahoo.com>
2016-05-30 21:02:18 -0500

    wrong spelling for louis room

--- app/example/scripts/EventsController.js
+++ app/example/scripts/EventsController.js
@@ -4,7 +4,7 @@ angular
     $scope.events = [
         {name:"Town Hall", date:"May 25th", location:"Tech A111"},
         {name:"Tamixer", date:"May 27th", location:"E2"},
-        {name:"Gala", date:"May 31st", location:"Norris Loius Room"}
+        {name:"Gala", date:"May 31st", location:"Norris Louis Room"}
     ]
     supersonic.ui.views.current.whenVisible(function() {
     	supersonic.ui.tabs.hide();
